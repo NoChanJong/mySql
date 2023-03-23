4eb95359313986d0aa7e2064681854dff815c022
No Chan Jong <kicc1963@gmail.com>
2023-03-23 19:47:41 +0900

    sql 4일차

--- 07.multi_function.sql
+++ 07.multi_function.sql
@@ -210,11 +210,265 @@ from PROFESSOR
 group by rollup(deptno, position);
 
 -- cube : rollup 함수와 다르게 합계를 먼저 표시한다
+-- 1. 부서별 직급별 사원수 평균급여
+-- 2. 부서별				사원수 평균급여
+-- 3. 			 직급별 사원수 평균급여
+-- 4. 전체					사원수 평균급여
 select deptno, position
 , count(*) 교수인원수
 , sum(pay)
 from PROFESSOR
-group by cube(deptno, position);
+group by cube(deptno, position)
+
+select deptno
+, job
+, count(*) 사원수
+, round(avg(sal + nvl(comm, 0)), 2) 평균급여
+from emp
+group by rollup(deptno, job);
+
+select deptno
+, job
+, count(*) 사원수
+, round(avg(sal + nvl(comm, 0)), 2) 평균급여
+from emp
+group by cube(deptno, job);
+
+/*
+	E. 순위함수
+	
+	1. rank()			  : 순위부여함수, 동일처리 1,2,2,4
+	2. dense_rank() : 순위부여함수, 동일처리 1,2,2,3
+	3. row_number() : 행번호를 제공해 주는 함수 동일처리는 불가 1,2,3,4
+	
+	[주의할 점]
+	순위함수는 반드시 order by절과 같이 사용해야 한다.
+*/
+-- 1. rank()
+-- 1) 특정자료별로 순위 : rank(조건값) within group(order by 조건값, 컬럼[asc|desc])
+-- 2) 전체자료기준 순위 : rank(조건) over(order by 조건값, 컬럼[asc|desc])
+
+-- 실습.
+-- 1) 특정조건의 순위
+-- SMITH사원이 알파벳순으로 몇번쨰인지
+select rownum, ename from emp;
+select rownum, emp.ename from emp;
+select rownum, e.ename from emp e;
+select rownum, ename from emp order by ename;
+select rownum, t1.rn, t1.ename from (select rownum rn, ename from emp order by ename) t1;
+
+select rank('SMITH') within group(order by ename) from emp;
+select rank('SMITH') within group(order by ename asc) from emp;
+select rank('SMITH') within group(order by ename desc) as 사원명 from emp;
+
+-- 2) 전체자료에서의 순위
+-- emp에서 각 사원들의 급여순위는?
+-- 급여가 작은순(asc), 급여가 많은순(desc)
+select * from emp order by sal;
+
+select ename, sal
+, rank() over(order by sal) -- 급여 적은순
+, rank() over(order by sal desc) -- 급여 많은순
+from emp;
+
+-- 2. dense_rank()
+select ename, sal
+, rank() over(order by sal) -- rank()
+, dense_rank() over(order by sal) -- dense_rank()
+from emp;
+
+-- 3. row_number() : 행번호
+select ename, sal
+, rank() over(order by sal) rank -- rank()
+, dense_rank() over(order by sal) dense_rank -- dense_rank()
+, row_number() over(order by sal) row_number -- row_number()
+from emp;
+
+/*
+	F. 누적함수
+	
+	1. sum(컬럼) over([partition by 컬럼...]order by 컬럼 [asc|desc]) : 누계(누적)를 구하는 함수
+	2. ratio_to_report() : 비율을 구하는 함수
+*/
+
+-- 1. sum() over()
+select * from panmae;
+select * from panmae where p_store=1000 order by 1;
+
+-- 1000대리점의 판매일자별 누계액 구하기
+select p_date
+, p_code
+, p_qty
+, p_total
+, sum(p_total) over(order by p_date) 일자별판매누계액 
+from panmae  
+where p_store=1000 order by 1;
+
+-- 판매일자별 제품별 판매누계액
+select * from panmae;
+
+select p_date
+, p_code
+, p_qty
+, p_total
+, sum(p_total) over(order by p_date, p_code) 판매누계액 
+from panmae  
+where p_store=1000 order by 1;
+
+-- 제품별/대리점별 기준으로 누계구하기(순서는 판매일자별)
+select p_code
+, p_store
+, p_date
+, p_total
+, sum(p_total) over(partition by p_code, p_store order by p_date) 판매누계액 
+from panmae;
+
+-- 2. ratio_to_report()
+-- 판매비율 
+select p_code
+, p_store
+, sum(p_qty) over() 총판매수량
+, round(ratio_to_report(sum(p_qty)) over() * 100, 2) "수량(%)"
+, p_total
+, sum(p_total) over() 총판매금액
+, round(ratio_to_report(sum(p_total)) over() * 100, 2) "금액(%)"
+from panmae
+group by p_code, p_qty, p_store, p_total;
+
+/* 연습문제 */
+-- 1. emp 테이블을 사용하여 사원 중에서 급여(sal)와 보너스(comm)를 합친 금액이 가장 많은 경우와 
+--    가장 적은 경우 , 평균 금액을 구하세요. 단 보너스가 없을 경우는 보너스를 0 으로 계산하고 
+--    출력 금액은 모두 소수점 첫째 자리까지만 나오게 하세요
+-- MAX, MIN, AVG
+select sal 급여
+, nvl2(comm, comm, 0)보너스
+, max(sal + nvl2(comm, comm, 0)) 가장많은경우
+, min(sal + nvl2(comm, comm, 0)) 가장적은경우
+, round(avg(sal + nvl2(comm, comm, 0))) 평균금액
+from emp
+group by sal, comm;
+
+-- 2. student 테이블의 birthday 컬럼을 참조해서 월별로 생일자수를 출력하세요
+-- TOTAL, JAN, ...,  5 DEC
+--  20EA   3EA ....
+select * from STUDENT;
+select to_char(BIRTHDAY) from STUDENT;
+
+select count(*) "생일자 합계"
+, count(case when to_char(BIRTHDAY, 'MM') ='01' then '1월' end) "1월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='02' then '2월' end) "2월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='03' then '3월' end) "3월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='04' then '4월' end) "4월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='05' then '5월' end) "5월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='06' then '6월' end) "6월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='07' then '7월' end) "7월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='08' then '8월' end) "8월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='09' then '9월' end) "9월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='10' then '10월' end) "10월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='11' then '11월' end) "11월 생일자"
+, count(case when to_char(BIRTHDAY, 'MM') ='12' then '12월' end) "12월 생일자"
+from STUDENT;
+
+-- 3. Student 테이블의 tel 컬럼을 참고하여 아래와 같이 지역별 인원수를 출력하세요.
+--    단, 02-SEOUL, 031-GYEONGGI, 051-BUSAN, 052-ULSAN, 053-DAEGU, 055-GYEONGNAM
+--    으로 출력하세요
+select count(*)  "합계"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='02' then 'SEOUL' end) "서울"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='031' then 'GYEONGGI' end) "경기"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='051' then 'BUSAN' end) "부산"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='052' then 'ULSAN' end) "울산"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='053' then 'DAEGU' end) "대구"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='055' then 'GYEONGNAM' end) "경남"
+from STUDENT;
+
+
+-- 4. emp 테이블을 사용하여 직원들의 급여와 전체 급여의 누적 급여금액을 출력,
+-- 단 급여를 오름차순으로 정렬해서 출력하세요.
+-- sum() over()
+select * from emp;
+
+select ename
+, sal 급여
+, sum(sal) over(order by sal) 누적급여금액
+from emp;
+
+-- 6. student 테이블의 Tel 컬럼을 사용하여 아래와 같이 지역별 인원수와 전체대비 차지하는 비율을 
+--    출력하세요.(단, 02-SEOUL, 031-GYEONGGI, 051-BUSAN, 052-ULSAN, 053-DAEGU,055-GYEONGNAM)
+select count(*)  "합계"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='02' then 'SEOUL' end) "서울"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='031' then 'GYEONGGI' end) "경기"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='051' then 'BUSAN' end) "부산"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='052' then 'ULSAN' end) "울산"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='053' then 'DAEGU' end) "대구"
+, count(case when substr(tel, 1, instr(tel, ')', 1, 1)-1)='055' then 'GYEONGNAM' end) "경남"
+, 
+from STUDENT;
+       
+
+-- 7. emp 테이블을 사용하여 부서별로 급여 누적 합계가 나오도록 출력하세요. 
+-- ( 단 부서번호로 오름차순 출력하세요. )
+select * from emp;
+
+select job
+, deptno
+, sum(sal) over(partition by job order by deptno desc)
+from emp;
+
+-- 8. emp 테이블을 사용하여 각 사원의 급여액이 전체 직원 급여총액에서 몇 %의 비율을 
+--    차지하는지 출력하세요. 단 급여 비중이 높은 사람이 먼저 출력되도록 하세요
+select deptno
+, ename 
+, sal 
+, sum(sal) over() 전체직원급여합계
+, round((ratio_to_report(sal) over()) * 100, 2)
+from emp
+group by deptno, ename, sal
+order by sal desc;
+
+-- 9. emp 테이블을 조회하여 각 직원들의 급여가 해당 부서 합계금액에서 몇 %의 비중을
+--     차지하는지를 출력하세요. 단 부서번호를 기준으로 오름차순으로 출력하세요.
+select * from emp;
+
+select deptno
+, ename
+, sal
+, sum(sal) over(order by deptno) 
+, round((ratio_to_report(sal) over())*100, 2)
+from emp
+group by deptno, ename, sal
+order by deptno desc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- grouping : 소계, 합계로 집계된 행의 컬럼 null을 구분할 수 있다.
 -- 						null인 경우 1을 반환하고 아닌경우 0을 반환한다.
